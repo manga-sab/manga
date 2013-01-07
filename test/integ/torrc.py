51362297303176909ca80d37aacfc4678c2c4692
Sabyasachi Shivkumar <sabyashiv@gmail.com>
2013-01-08 01:20:20 +0530

    Added testing of invalid inputs

--- test/integ/torrc.py
+++ test/integ/torrc.py
@@ -7,16 +7,14 @@ VALID_TORRC = """Softsport 9020
 redsport 1203\n\n\n
 dffport 1302\n\n\n\n\n"""
 
-
+INVALID_TORRC = """Congosport"""
 class TestTorrcparser(unittest.TestCase):
   def test_torrc_parser(self):
      torrc_file=open("textfile",'w')
      torrc_file.write(VALID_TORRC)
      torrc_file.close()
-     torrc_file=open("textfile")
      parsed_torrc=stem.torrc.parse_torrc("textfile")
      torrc_file.close()
-     #os.chdir("/home/sabyashiv/stem/test/integ")
      os.remove("textfile")
      torrc_ori={
          "Softsport": "9020",
@@ -26,4 +24,13 @@ class TestTorrcparser(unittest.TestCase):
      for key in parsed_torrc :
        self.assertEqual(parsed_torrc[key],torrc_ori[key])
 
-
+  def test_invalid_input(self):
+    torrc_file=open("textfile",'w')
+    torrc_file.write(INVALID_TORRC)
+    torrc_file.close()
+    torrc_file=open("textfile")
+    self.assertRaises(stem.torrc.INVALID_INPUT, stem.torrc.parse_torrc, "textfile")
+    torrc_file.close()
+    os.remove("textfile")
+    
+    
