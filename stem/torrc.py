5f18ab21dad231e9d928a7d6f4cd418909799378
Sabyasachi Shivkumar <sabyashiv@gmail.com>
2013-01-09 12:17:26 +0530

    Changed parser to accept files and Adjusted tests
    The torrc parser was changed to accept files.Also the write and parser were madeas member functions.The tests were modified accordingly.

--- stem/torrc.py
+++ stem/torrc.py
@@ -1,3 +1,4 @@
+from __future__ import with_statement
 import tempfile
 
 """
@@ -8,10 +9,9 @@ Torrc information.
   >>> import stem.torrc
   >>> torrc = stem.torrc.Torrc()
   >>> torrc['controlport'] = 9051
-  >>> write_torrc(torrc)
-  '/var/folders/8r/0cd26s_d7dqgqkf5sbsgf4vh0000gn/T/torrc-D_I79n'
-
-  >>> torrc = parse_torrc('/var/folders/8r/0cd26s_d7dqgqkf5sbsgf4vh0000gn/T/torrc-D_I79n')
+  >>> torrc.write('/var/folders/8r/0cd26s_d7dqgqkf5sbsgf4vh0000gn/T/torrc-D_I79n')
+  
+  >>> torrc._parse('/var/folders/8r/0cd26s_d7dqgqkf5sbsgf4vh0000gn/T/torrc-D_I79n')
   >>> torrc
   {'controlport': '9051'}
   >>> print torrc
@@ -21,63 +21,52 @@ Torrc information.
 
 ::
 
-  parse_torrc - parses a torrc file and returns a Torrc instance
-  write_torrc - writes the Torrc into a file
-
+  Torrc._parse - parses a torrc file and returns a Torrc instance
+  Torrc.write - writes the Torrc into a file
+  
   Torrc - Torrc information
     +- __str__ - string representation
 """
-
-def parse_torrc(torrc_path=None):
-  """
-  Returns a Torrc instance with all the key value mappings in a torrc
-  
-  :param str msg: path to torrc file
-  
-  :returns: **Torcc** key value mappings of torrc
-  """
-  
-  torrc = Torrc()
-  
-  if not torrc_path:
-    raise ValueError("Require Torrc")
-  
-  with open(torrc_path) as torrc_fh:
-    for line in torrc_fh.readlines():
-      line = line.strip()      
-      if line.startswith('#'):
-        continue
-      elif line=="":
-        continue
-      else:
-        key, val = line.split(' ')
-        torrc[key] = val
-  
-  return torrc
-
-def write_torrc(config, torrc_path=None):
-  """
-  Creates a torrc file.
-
-  :param Torrc config: config options for torrc
-  :param str torrc_path: path to make new torrc file
-  
-  :returns: **str** path to the new torrc
-  """
-  
-  if not torrc_path:
-      torrc_path = tempfile.mkstemp(prefix = "torrc-", text = True)[1]
-  
-  with open(torrc_path, "w") as torrc_file:
-    for key, value in config.items():
-      torrc_file.write("%s %s\n" % (key, value))
-  
-  return torrc_path
+class InvalidInput(Exception): pass
 
 class Torrc(dict):
-  def __init__(self):
+  def __init__(self, torrc_file=None):
     super(Torrc, self).__init__()
+    if torrc_file:
+      self._parse(torrc_file)
   
   def __str__(self):
     return '\n'.join(["%s %s" % (key, value) for key, value in self.items()])
   
+  def _parse(self, torrc_file):
+    """
+    Returns a Torrc instance with all the key value mappings in a torrc
+    
+    :param file :  torrc file
+    
+    :returns: **Torcc** key value mappings of torrc
+    """
+    
+    for line in torrc_file:
+      line = line.strip()
+      if not (line.startswith('#') or line==""):
+        try:
+          key, val = line.split(None, 1)
+        except:
+          raise(InvalidInput())
+        self[key] = val
+  
+  def write(self, path):
+    """
+    Creates a torrc file.
+    
+    :param Torrc config: config options for torrc
+    :param str torrc_path: path to make new torrc file
+    
+    :returns: **str** path to the new torrc
+    """
+    
+    with open(path, "w") as torrc_file:
+      torrc_file.write(str(self))
+
+
--- test/integ/torrc.py
+++ test/integ/torrc.py
@@ -2,6 +2,8 @@ import stem.torrc
 import unittest
 import os
 
+from StringIO import StringIO
+
 VALID_TORRC = """Softsport 9020
 #Flasaport 7839\n\n
 redsport 1203\n\n\n
@@ -10,27 +12,24 @@ dffport 1302\n\n\n\n\n"""
 INVALID_TORRC = """Congosport"""
 class TestTorrcparser(unittest.TestCase):
   def test_torrc_parser(self):
-     torrc_file=open("textfile",'w')
-     torrc_file.write(VALID_TORRC)
-     torrc_file.close()
-     parsed_torrc=stem.torrc.parse_torrc("textfile")
-     torrc_file.close()
-     os.remove("textfile")
+     """
+     Checks the proper writing and accurate retrieval of the parser
+     """
+     parsed_torrc=stem.torrc.Torrc(StringIO(VALID_TORRC))
      torrc_ori={
          "Softsport": "9020",
          "redsport": "1203",
          "dffport": "1302",
                }
      for key in parsed_torrc :
+       #To verify the parsed data matches the original data
        self.assertEqual(parsed_torrc[key],torrc_ori[key])
-
+  
   def test_invalid_input(self):
-    torrc_file=open("textfile",'w')
-    torrc_file.write(INVALID_TORRC)
-    torrc_file.close()
-    torrc_file=open("textfile")
-    self.assertRaises(stem.torrc.INVALID_INPUT, stem.torrc.parse_torrc, "textfile")
-    torrc_file.close()
-    os.remove("textfile")
+    """
+    Checks invalid inputs in the file to be parsed
+    """
+    test=stem.torrc.Torrc()
+    self.assertRaises(stem.torrc.InvalidInput, test._parse, StringIO(INVALID_TORRC))
     
     
